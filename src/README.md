937131c04b78823fdd3d151879948a2a047c405c
Ken Huh <ken123777@gmail.com>
2017-11-07 13:52:05 +0900

    Update data

--- src/README.md
+++
@@ -1,6 +0,0 @@
-![Vanilla Coding](https://s3.ap-northeast-2.amazonaws.com/vanilla-coding/Assets/logo_regular%403x.png)
-
-## News Search System
-
-* [NY Times API](https://developer.nytimes.com/)
-* [APIGEE Console](https://apigee.com/console/nytimes)
--- src/index.js
+++ src/index.js
@@ -2,8 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './components/Home.jsx';
 import './index.css';
+import axios from 'axios';
+
+// ========================================
 
 ReactDOM.render(
   <Home />,
   document.getElementById('root')
 );
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+axios.get(url, {
+  params: {
+    'api-key': "ddebb6e03d6f49648bdb0830ba37a061",
+    'q': "seoul",
+    'begin_date': "19880917",
+    'end_date': "19880918",
+    'sort': "newest"
+  }
+})
+.then(function (response) {
+  console.log(JSON.stringify(response.data.response.docs));
+})
+.catch(function (error) {
+  console.error(error);
+});
