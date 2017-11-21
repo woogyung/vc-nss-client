9e2b7d5cb9e48b70bb1159098cf52b424ea103bf
Ken Huh <ken123777@gmail.com>
2017-11-21 17:55:57 +0900

    complete v0 skeleton

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import articlesData from '../config/data.json';
 import Article from './Article.jsx';
 
+/* Formatting date */
+
+// 1. Inspect current date format
+
+// 2. Write a utility function
+
+// 3. Render pretty format
+
+// 4. Test app
+
+// 5. Re-write using moment.js
+
 export default class Home extends React.Component {
   render() {
     return (
