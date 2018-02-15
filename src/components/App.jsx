e9cc74f50d34a54cf77bce8c69e18a2c85d0ef1f
myeongwon <lighting_one@hanmail.net>
2018-02-15 13:44:04 +0900

    [완성] 코드정리, 수정..

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,78 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import articlesData from '../config/data.json';
 import Article from './Article.jsx';
-import OrderButtons from './OrderButtons.jsx';
 
 export default class App extends React.Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
-      articlesData: articlesData,
-      dataCount: 0,
+      articlesData : articlesData,
     }
-  }
+  };
 
-  AscendingOrder(){
-    // 오름차순, 최신순
-    if(this.state.dataCount !== 0){
-      this.state.articlesData = articlesData.reverse();
-      this.state.dataCount = 0;
-    }
+  asending(){
+    const articlesCopy = this.state.articlesData.slice();
+
+    articlesCopy.sort(function (a,b){
+      return new Date(b.published_date) - new Date(a.published_date);
+    });
 
     this.setState({
-        articlesData: articlesData
+      articlesData: articlesCopy
     });
   }
 
-  DescendingOrder(){
-    // 내림차순, 과거순...
-    this.state.articlesData = articlesData.reverse();
+  desending(){
+    const articlesCopy = this.state.articlesData.slice();
 
-    if(this.state.dataCount !== 0){
-      this.state.articlesData = articlesData.reverse();
-      return;
-    }
-    this.state.dataCount++;
+    articlesCopy.sort(function (a,b){
+      return new Date(a.published_date) - new Date(b.published_date);
+    });
 
     this.setState({
-      articlesData: articlesData,
+      articlesData: articlesCopy
     });
+
   }
 
-  renderOrderButtons(){
-    return (
-      <div className="btns">
-        <OrderButtons
-          value=" 오름차순 "
-          onClick={ () => this.AscendingOrder() }
-        />
-        <OrderButtons
-          value=" 내림차순 "
-          onClick={ () => this.DescendingOrder() }
-        />
-      </div>
-    );
-  }
-
-  renderArticles(){
-    return(
-      articlesData.map((data, i) => {
-        return <Article
-          url={data.short_url}
-          mainHeadline={data.title}
-          key={i}
-          thumbnailURL={data.multimedia.length ? data.multimedia[1].url : null}
-          publishedDate={data.published_date}
-        />
-      })
-    );
-  }
 
   render() {
     return (
       <div className="home">
-        { this.renderOrderButtons() }
-        { this.renderArticles() }
+        <div>
+          <button onClick={() => this.asending()} >최신순</button>
+          <button onClick={() => this.desending()} >오래된순</button>
+        </div>
+        {
+          this.state.articlesData.map((data, i) => {
+            return <Article
+              url={data.short_url}
+              mainHeadline={data.title}
+              key={i}
+              thumbnailURL={data.multimedia.length ? data.multimedia[1].url : null}
+              publishedDate={data.published_date}
+            />
+          })
+        }
       </div>
     );
   }
