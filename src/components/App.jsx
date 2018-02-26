eb4752d6eb3a5bebaf27210fb9d591c05ddb9c9f
gyu <gyu.will85@gmail.com>
2018-02-26 22:17:11 +0900

    gyu-v2-login 1st commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,28 +2,114 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import articlesData from '../config/data.json';
 import Article from './Article.jsx';
+import Login from './Login.jsx'
 import axios from 'axios';
 
 /* Day 7: Signup, Login, Logout */
 
-// 1. Create Login Form UI Component
+// 1. Create Login Form UI Component checked
 
-// 2. Make POST /signup Request to the API
+// 2. Make POST /signup Request to the API checked
 
-// 3. Make POST /login Request to the API
+// 3. Make POST /login Request to the API checked
 
-// 4. Hide other views if not logged in
+// 4. Hide other views if not logged in done
 
 // 5. Password secured?
 
 // 6. Test app
 
+const logInfo = {
+    userName: '',
+    passWord: ''
+};
+
 export default class App extends React.Component {
+  
+  constructor() {
+    super();
+    
+    this.state = {
+        style: {
+          display:'none'
+        },
+        logInfoMessage:'',
+        logIn: false
+    }
+  };
+  
+  onConfirm() {
+    const loginValue = document.querySelectorAll('.inp-inform');
+    
+    for(let i = 0; i < loginValue.length; i++) {
+        logInfo.userName = loginValue[0].value;
+        logInfo.passWord = loginValue[1].value;
+    };
+    
+    axios.post('http://localhost:8081/login', {
+      username: logInfo.userName,
+      password: logInfo.passWord
+    })
+    .then((response) => {
+      this.setState({
+        login:true
+      })
+    })
+    .catch((error) => {
+      this.setState({
+          style: {
+            display:'block',
+            color: '#DF013A'
+          },
+          logInfoMessage: error.response.data.message
+        });
+    });
+  }
+
+  onJoin () {
+    const loginValue = document.querySelectorAll('.inp-inform');
+
+    for(let i = 0; i < loginValue.length; i++) {
+        logInfo.userName = loginValue[0].value;
+        logInfo.passWord = loginValue[1].value;
+    };
+
+    axios.post('http://localhost:8081/signup', {
+        username: logInfo.userName,
+        password: logInfo.passWord
+    })
+    .then( (response) => {
+        this.setState({
+          style: {
+            display:'block',
+            color: '#2E2EFE'
+          },
+          logInfoMessage: response.data.message,
+          login:true
+        });
+    })
+    .catch( (error) => {
+      this.setState({
+          style: {
+            display:'block',
+            color: '#DF013A'
+          },
+          logInfoMessage: error.response.data.message
+        });
+    });
+  };
+  
   render() {
     return (
       <div className="home">
-        {
-          articlesData.map((data, i) => {
+        { 
+          !this.state.login && <Login 
+            logInfoMessage = { this.state.logInfoMessage }
+            style = { this.state.style}
+            onJoin ={ this.onJoin.bind(this)}
+            onConfirm = { this.onConfirm.bind(this) }
+          /> ||
+          this.state.login && articlesData.map((data, i) => {
             return <Article
               url={data.short_url}
               mainHeadline={data.title}
@@ -32,7 +118,7 @@ export default class App extends React.Component {
               publishedDate={data.published_date}
             />
           })
-        }
+        }    
       </div>
     );
   }
---
+++ src/components/Login.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+    render () {
+        return (
+          <div className="login">
+            <form action="#">
+              <fieldset>
+                  <legend>로그인 화면 입니다.</legend>
+                  <div className="box-inform">
+                    <span className="txt-inform">ID</span>
+                    <input id="logId" type="text" className = "inp-inform" />
+                  </div>
+                  <div className="box-inform">
+                    <span className="txt-inform">Password</span>
+                    <input id="logPassword" type="password" className ="inp-inform" />
+                  </div>
+                  <div className="box-button">
+                    <button type="submit" className="btn" onClick = { () => this.props.onConfirm() }>확인</button>
+                    <button type="reset" className="btn">취소</button>
+                    <button type="submit" className="btn join" onClick = { () => this.props.onJoin() }>회원가입</button>
+                  </div>
+                  <p className="message" style={this.props.style}>{ this.props.logInfoMessage }</p>
+              </fieldset>
+            </form>
+          </div>
+        ) 
+    }
+}
