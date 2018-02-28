29c95c31d5b6b2f9102d3218757149a455bfd57b
unknown <yzkim_office@naver.com>
2018-02-28 15:13:37 +0900

    세미콜론 빠진 부분 추가

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,58 +4,55 @@ import Article from './Article.jsx';
 
 export default class App extends React.Component {
 
-  constructor() {
-      super();
-      this.state = {
-          articles : articlesData,
-          btnNew : null,
-          btnOld : null
-      }
-  }
-
-  sortNews(sortType) {
-    const articles = this.state.articles.slice();
-
-    if(sortType === 'newest') {
-        articles.sort(function (a,b) {
-            return new Date(b.published_date) - new Date(a.published_date);
-        });
-        this.setState({
-            articles : articles,
-            btnNew : "active",
-            btnOld : null
-        });
-    } else {
-        articles.sort(function (a,b) {
-            return new Date(a.published_date) - new Date(b.published_date);
-        });
-        this.setState({
-            articles : articles,
-            btnNew : null,
-            btnOld : "active"
-        });
+    constructor() {
+        super();
+        this.state = {
+            articles: articlesData,
+            btnNew: null,
+            btnOld: null
+        };
     }
-  }
 
-  render() {
-    return (
-      <div className="home">
-          <div className="btns">
-              <button className={this.state.btnNew} onClick={() => this.sortNews('newest')}>Newest</button>
-              <button className={this.state.btnOld} onClick={() => this.sortNews('oldest')}>Oldest</button>
-          </div>
-        {
-          this.state.articles.map((data, i) => {
-            return <Article
-              url={data.short_url}
-              mainHeadline={data.title}
-              key={i}
-              thumbnailURL={data.multimedia.length ? data.multimedia[1].url : null}
-              publishedDate={data.published_date}
-            />
-          })
+    sortNews(sortType) {
+        const articles = this.state.articles.slice();
+
+        if (sortType === 'newest') {
+            articles.sort(function(a, b) {
+                return new Date(b.published_date) - new Date(a.published_date);
+            });
+            this.setState({
+                articles: articles,
+                btnNew: "active",
+                btnOld: null
+            });
+        } else {
+            articles.sort(function(a, b) {
+                return new Date(a.published_date) - new Date(b.published_date);
+            });
+            this.setState({
+                articles: articles,
+                btnNew: null,
+                btnOld: "active"
+            });
         }
-      </div>
-    );
-  }
+    }
+
+    render() {
+        return (<div className="home">
+            <div className="btns">
+                <button className={this.state.btnNew} onClick={() => this.sortNews('newest')}>Newest</button>
+                <button className={this.state.btnOld} onClick={() => this.sortNews('oldest')}>Oldest</button>
+            </div>
+            {
+                this.state.articles.map((data, i) => {
+                    return <Article
+                        url={data.short_url}
+                        mainHeadline={data.title}
+                        key={i}
+                        thumbnailURL={data.multimedia.length ? data.multimedia[1].url : null}
+                        publishedDate={data.published_date}/>
+                })
+            }
+        </div>);
+    }
 }
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -3,27 +3,27 @@ import moment from 'moment';
 import 'moment/locale/ko'
 
 export default class Article extends React.Component {
-  render() {
-    return (
-      <div className="article">
-        {
-          this.props.publishedDate &&
-          <div className="article-date">
-            <p>{moment(this.props.publishedDate).format('llll')}</p>
-          </div>
-        }
-        <div className="image-panel">
-          {
-            this.props.thumbnailURL && <img src={this.props.thumbnailURL} alt="" />
-          }
-          <div>
-            <a href={ this.props.url } target="_blank" alt="">Link</a>
-          </div>
-        </div>
-        <div className="article-panel">
-          <p>{ this.props.mainHeadline }</p>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="article">
+                {
+                    this.props.publishedDate &&
+                    <div className="article-date">
+                        <p>{moment(this.props.publishedDate).format('llll')}</p>
+                    </div>
+                }
+                <div className="image-panel">
+                    {
+                        this.props.thumbnailURL &&
+                        <img src={this.props.thumbnailURL} alt=""/>
+                    }
+                    <div>
+                    <a href={this.props.url} target="_blank" alt="">Link</a>
+                </div>
+            </div>
+            <div className="article-panel">
+                <p>{this.props.mainHeadline}</p>
+            </div>
+        </div>);
+    }
 }
