e96ef0eeedc128f16c69b9af1c6497e969fd200e
woogyung <whdnrud93@naver.com>
2018-03-02 17:22:32 +0900

    [완료] 새로운 데이터 받아오기 및 스피너 추가

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,45 +21,119 @@ export default class App extends React.Component {
     super();
     
     this.state={
+      showLogin: true,
       articlesData: articlesData,
+      username: "",
+      password: "",
+      spinner: false
     }
   }
   
   onClickSection(ev) {
     const sectionName = ev.target.textContent;
+
+    this.setState({
+      spinner: true
+    });
     
     axios.get(`http://localhost:8081/top-stories/${sectionName}`, {
       headers: {
-        'Authorization': 'Bearer ghs__df=423njsfdruur12jfjuh4!?frt34563ju8h84h2d10' 
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ijc4OSIsImlhdCI6MTUxOTkyMDUxMCwiZXhwIjoxNTIwMDA2OTEwfQ.oKYJCuknscyAePv3oW9rogaGNatXFCyfA1h74P6p2h4"
       }
     })
       .then((response) => {
-        console.log('성공');
+        console.log("성공");
         this.setState({
+          spinner: false,
           articlesData: response.data.results
         });
       })
       .catch((error) => {
-        console.log('실패');
+        console.log("실패");
         this.setState({
+          spinner: false,
           articlesData: articlesData
         });
       })
   }
   
+  onUsernameChange(ev) {
+    this.setState({
+      username: ev.target.value,
+      password: ev.target.value
+    });
+  }
+  
+  onPasswordChange(ev) {
+    this.setState({
+      username: ev.target.value,
+      password: ev.target.value
+    });
+  }
+  
+  onLoginClick(ev) {
+    console.log("click");
+    
+    axios.post("http://localhost:8081/login", {
+      username: this.state.username,
+      password: this.state.password
+    })
+    
+    .then((response) => {
+      console.log("성공");
+      
+      this.setState({
+        showLogin: false,
+        articlesData: articlesData
+      });
+    })
+    
+    .catch((error) => {
+      console.log("에러");
+      
+      this.setState({
+        showLogin: true,
+        articlesData: null
+      });
+    })
+  }
+  
   render() {
     return ( 
       <div className="home">
-        
-      <div>
-        <ul onClick={this.onClickSection.bind(this)}>
-          <li>fashion</li>
-          <li>business</li>
-          <li>tmagazine</li>
-        </ul>  
-      </div> 
-        
+        { 
+          (!this.state.showLogin && this.state.spinner) && // 로그인 모드일때와 새로운 데이타를 받아보기 전에는 보이면 단됨.
+
+          <div className="holder">
+            <div className="preloader">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>  
+          </div>
+        }
         {
+          (this.state.showLogin) &&
+          <div>
+            <input type="text" onChange={this.onUsernameChange.bind(this)}/>
+            <input type="text" onChange={this.onPasswordChange.bind(this)}/>
+            <button onClick={this.onLoginClick.bind(this)}>Login</button>  
+          </div>
+        }
+        {
+          (!this.state.showLogin) &&
+          <div>
+            <ul onClick={this.onClickSection.bind(this)}>
+              <li>fashion</li>
+              <li>business</li>
+              <li>tmagazine</li>
+            </ul>  
+          </div>
+        }
+        {
+          (!this.state.showLogin) &&
           this.state.articlesData.map((data, i) => {
             return <Article
               url={data.short_url}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import './index.css';
+import './spinner.css';
 
 // ========================================
 
