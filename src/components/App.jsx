54ad0d39c95aa2a798e560ac301fa080a4db508e
yzkim-office <yzkim_office@naver.com>
2018-03-06 16:03:41 +0900

    작업중

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import articlesData from '../config/data.json';
 import Article from './Article.jsx';
+import Login from './Login.jsx';
 import axios from 'axios';
 
 /* Day 6: Connecting NY Times data */
@@ -18,11 +18,54 @@ import axios from 'axios';
 // 5. Re-render upon receiving new data
 
 export default class App extends React.Component {
+
+  constructor() {
+        super();
+
+        this.state = {
+            loginComplete : false,
+            articlesData : articlesData
+        };
+    }
+
+    checkLoginState(){
+        this.setState({
+            loginComplete : true
+        });
+     }
+
+
+  onCategorySelect(e) {
+    const categoryName = e.target.textContent;
+
+    axios.get('http:localhost:8081/top-stories/${categoryName}',{
+        headers : {
+            Authorization: 'Bearer ghs__df=423njsfdruur12jfjuh4!?frt34563ju8h84h2d10'
+        }
+    })
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((error) => {
+        console.table(error);
+    });
+  }
+
   render() {
     return (
       <div className="home">
+          <div>
+              <ul onClick={this.onCategorySelect.bind(this)}>
+                  <li>upshot</li>
+                  <li>travel</li>
+                  <li>fashion</li>
+                  <li>books</li>
+              </ul>
+          </div>
+        <Login checkLogin={this.checkLoginState.bind(this)} />
         {
-          articlesData.map((data, i) => {
+            this.state.loginComplete &&
+            this.state.articlesData.map((data, i) => {
             return <Article
               url={data.short_url}
               mainHeadline={data.title}
---
+++ src/components/Login.jsx
@@ -0,0 +1,197 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state={
+            userId : "",
+            userPw : "",
+            loginFormShow : true,
+            joinFormShow : false,
+            submitResult : "",
+            isLoggedIn : false
+        };
+    }
+
+    handleChange(e) {
+        const formName = e.target.name;
+        const formValue = e.target.value;
+
+        this.setState({
+            [formName] : formValue
+        });
+    }
+
+    validate(type) {
+        const userId = this.state.userId;
+        const userPw = this.state.userPw;
+        let valResult = null;
+
+        if(userId && userPw) {
+            return true;
+        } else {
+            if(!userId && !userPw) {
+               valResult ="id, pw를 입력해주세요"
+           } else if (!userId) {
+               valResult = "id를 입력해주세요";
+           } else if (!userPw) {
+               valResult = "pw를 입력해주세요";
+           }
+
+           if (type === 'submitJoin') {
+               this.setState({
+                   submitResult : valResult
+               });
+               return false;
+           } else if (type === 'submitLogin') {
+               this.setState({
+                   submitResult : valResult
+               });
+               return false;
+           }
+        }
+    }
+
+    submitJoin(e) {
+        const that = this;
+        e.preventDefault();
+        if(this.validate('submitJoin')){
+            axios.post('http://localhost:8081/signup', {
+                username : this.state.userId,
+                password : this.state.userPw
+            })
+            .then(function (response) {
+                const result = response;
+                if(result.status === 201) {
+                    that.setState({
+                        loginFormShow : true,
+                        joinFormShow : false,
+                        userId : "",
+                        userPw : "",
+                        submitResult : "회원가입에 성공하였습니다. 로그인해주세요"
+                    });
+                }
+            })
+            .catch(function (error) {
+                that.setState({
+                    submitResult : "id와 password를 확인해주세요"
+                });
+            })
+        }
+    }
+
+    submitLogin(e) {
+        e.preventDefault();
+         if(this.validate('submitLogin')){
+             axios.post('http://localhost:8081/login', {
+                 username : this.state.userId,
+                 password : this.state.userPw
+             })
+             .then( (response) => {
+                 if(response.status === 200) {
+
+                     const userToken = response.data.access_token;
+                     console.log(userToken)
+                     const userId = response.data.data.username;
+                     this.setState({
+                         loginFormShow : false,
+                         joinFormShow : false,
+                         isLoggedIn : true,
+                         userId : userId
+                     });
+                    this.props.checkLogin();
+                 }
+             })
+             .catch( (error) => {
+                 this.setState({
+                     submitResult : "id와 password를 확인해주세요"
+                 });
+             })
+         }
+    }
+
+    joinBtnClick(e) {
+        e.preventDefault();
+        this.setState({
+            loginFormShow : false,
+            joinFormShow : true,
+            submitResult : ""
+        });
+    }
+
+    loginBtnClick(e) {
+        e.preventDefault();
+        this.setState({
+            loginFormShow : true,
+            joinFormShow : false,
+            submitResult : ""
+        });
+    }
+
+  render() {
+    return (
+
+    <div className="account_form">
+        {
+            this.state.loginFormShow &&
+            <div className="login">
+                <form>
+                    <fieldset>
+                        <legend>로그인</legend>
+                        <dl>
+                            <dt>ID</dt>
+                            <dd>
+                                <label><input type="text" value={this.state.userId} placeholder="ID" name="userId" onChange={this.handleChange.bind(this)} /></label>
+                            </dd>
+                            <dt>PASSWORD</dt>
+                            <dd>
+                                <label><input type="password" value={this.state.userPw} placeholder="PASSWORD" name="userPw" onChange={this.handleChange.bind(this)} /></label>
+                            </dd>
+                        </dl>
+                        <p className="validate_txt">{this.state.submitResult}</p>
+                        <button type="submit" onClick={(e) => this.submitLogin(e)}>로그인</button>
+                        <a href="" onClick={this.joinBtnClick.bind(this)}>회원가입</a>
+                    </fieldset>
+                </form>
+            </div>
+        }
+
+        {
+            this.state.joinFormShow &&
+            <div className="join">
+                <form>
+                    <fieldset>
+                        <legend>회원가입</legend>
+                        <dl>
+                            <dt>ID</dt>
+                            <dd>
+                                <label><input type="text" value={this.state.userId} placeholder="ID" name="userId" onChange={this.handleChange.bind(this)} /></label>
+                            </dd>
+                            <dt>PASSWORD</dt>
+                            <dd><label><input type="password" value={this.state.userPw} placeholder="PASSWORD" name="userPw" onChange={this.handleChange.bind(this)} /></label></dd>
+                        </dl>
+                        <p className="validate_txt">{this.state.submitResult}</p>
+                        <button type="submit" onClick={this.submitJoin.bind(this)}>회원가입</button>
+                        <a href="" onClick={this.loginBtnClick.bind(this)}>로그인</a>
+                    </fieldset>
+                </form>
+            </div>
+        }
+
+        {
+            this.state.isLoggedIn &&
+            <div className="greetings">
+                <img src="http://thumbnail.egloos.net/600x0/http://pds25.egloos.com/pds/201510/17/68/b0245168_56215dcf51546.png" alt="" />
+                <div>
+                    <span>{this.state.userId}</span>
+                    님 안녕하세요!
+                </div>
+            </div>
+        }
+
+    </div>
+    );
+  }
+}
