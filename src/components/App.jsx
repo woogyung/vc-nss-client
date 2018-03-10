e50fa1d8168f6dca22f8fe6d019f6ebd8b14fb75
woogyung <whdnrud93@naver.com>
2018-03-10 19:05:21 +0900

    에러 수정

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import articlesData from '../config/data.json';
 import Article from './Article.jsx';
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export default class Article extends React.Component {
   render() {
